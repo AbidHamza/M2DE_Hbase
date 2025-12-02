beab479a61d34c9231e6346180e9389c6867f327
AbidHamza <abid.hamzadev@gmail.com>
2025-12-02 11:52:59 +0100

    Fix JAVA_HOME partout + Restructuration complÃ¨te avec explication dÃ©taillÃ©e du dÃ©pÃ´t

--- docker/hbase/hbase-env.sh
+++ docker/hbase/hbase-env.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/opt/java/openjdk
 export HBASE_HEAPSIZE=512
 export HBASE_OPTS="$HBASE_OPTS -XX:+UseConcMarkSweepGC"
 export HBASE_MANAGES_ZK=false
