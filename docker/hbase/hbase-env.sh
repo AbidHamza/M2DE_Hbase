41266b712bfef5c0feedd7b8e9c03369a775950d
AbidHamza <abid.hamzadev@gmail.com>
2025-12-02 15:39:29 +0100

    Fix HBase: correction JAVA_HOME dynamique et fins de ligne Windows dans hbase-env.sh

--- docker/hbase/hbase-env.sh
+++ docker/hbase/hbase-env.sh
@@ -1,5 +1,10 @@
-export JAVA_HOME=/opt/java/openjdk
+#!/bin/bash
+# Detect JAVA_HOME dynamically if not set
+if [ -z "$JAVA_HOME" ] || [ ! -d "$JAVA_HOME" ]; then
+    export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+fi
+export PATH=${JAVA_HOME}/bin:${PATH}
+
 export HBASE_HEAPSIZE=512
 export HBASE_OPTS="$HBASE_OPTS -XX:+UseConcMarkSweepGC"
 export HBASE_MANAGES_ZK=false
-
