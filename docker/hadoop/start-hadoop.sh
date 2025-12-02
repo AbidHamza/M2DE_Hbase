d0505f1e8da1ee288e5963524f989120e85ceeed
AbidHamza <abid.hamzadev@gmail.com>
2025-12-02 12:04:23 +0100

    Fix JAVA_HOME: correction chemin pour eclipse-temurin (temurin-8-jdk-amd64)

--- docker/hadoop/start-hadoop.sh
+++ docker/hadoop/start-hadoop.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Set JAVA_HOME if not already set
-export JAVA_HOME=${JAVA_HOME:-/opt/java/openjdk}
+# Set JAVA_HOME - Try multiple possible locations
+if [ -z "$JAVA_HOME" ]; then
+    if [ -d "/usr/lib/jvm/temurin-8-jdk-amd64" ]; then
+        export JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
+    elif [ -d "/opt/java/openjdk" ]; then
+        export JAVA_HOME=/opt/java/openjdk
+    else
+        export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+    fi
+fi
+
 export PATH=${JAVA_HOME}/bin:${PATH}
+
+# Verify JAVA_HOME
+echo "JAVA_HOME is set to: $JAVA_HOME"
+if [ ! -f "$JAVA_HOME/bin/java" ]; then
+    echo "ERROR: JAVA_HOME is incorrect: $JAVA_HOME"
+    exit 1
+fi
 
 # Start SSH
 service ssh start
--- docker/hbase/hbase-env.sh
+++ docker/hbase/hbase-env.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/opt/java/openjdk
+export JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
 export HBASE_HEAPSIZE=512
 export HBASE_OPTS="$HBASE_OPTS -XX:+UseConcMarkSweepGC"
 export HBASE_MANAGES_ZK=false
